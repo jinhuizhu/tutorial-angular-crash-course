5fe4b66f48935be8ac3c31b3f0ebd4f8905ab83b
Jinhui Zhu <jinhuizhu@gmail.com>
2019-12-24 21:10:51 +0800

    clean up weight-entries service

--- src/app/weight-entries.service.ts
+++ src/app/weight-entries.service.ts
@@ -7,31 +7,7 @@ import {map} from "rxjs/operators";
   providedIn: 'root'
 })
 export class WeightEntriesService {
-  public entriesArray: Entry[] = [
-    {id:1,date:new Date('1/1/1996'),weight:165,bodyfat:.20},
-    {id:2,date:new Date('1/2/1996'),weight:164,bodyfat:.19},
-    {id:3,date:new Date('1/3/1996'),weight:164,bodyfat:.19},
-    {id:4,date:new Date('1/12/1996'),weight:161,bodyfat:.18},
-    {id:5,date:new Date('12/31/1995'),weight:167,bodyfat:.20},
-    {id:6,date:new Date('12/1/1995'),weight:161,bodyfat:.18},
-  ]
-
-  public sortedEntries: Entry[];
-
   constructor(private http: HttpClient) {
-    this.sortEntries();
-  }
-
-  private sortEntries() {
-    this.sortedEntries = [...this.entriesArray].sort((a:Entry, b: Entry) => {
-      if (a.date > b.date) {
-        return 1;
-      } else if (a.date.getTime() == b.date.getTime()) {
-        return 0;
-      } else {
-        return -1;
-      }
-    });
   }
 
   /**
